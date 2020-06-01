eafbcde8128ade2254d21a45ad1856b9ae01b62d
hermlon <dev.hermlon@yamx.net>
2020-06-01 22:42:24 +0200

    added interactive mode with single password prompt

--- backup.fish
+++ backup.fish
@@ -7,29 +7,38 @@ read -slx -P "Password: " RESTIC_PASSWORD
 if restic snapshots >/dev/null
 	# TODO: don't redirect to /dev/null but parse and show last backup date
 	# repository opened successfully
-	set backup_start_time (date +%s)
-	restic backup \
-		--verbose \
-		--files-from ~/.config/restic/backup.files \
-		--exclude-file ~/.config/restic/exclude.files
-	
-	echo "Forgetting good old times..."
-	restic forget \
-		--prune \
-		--keep-last 3 \
-		--keep-daily 14 \
-		--keep-monthly 6 \
-		--keep-yearly 1000
+	echo "Successfully opened repository"
 
-	restic check
+	# TODO: add prompt in interactive mode (like in venv/bin/activate.fish)
+	argparse "i/interactive" -- $argv
+	if set -q _flag_i
+		# run fish shell with exported restic password
+		fish
+	else
+		set backup_start_time (date +%s)
+		restic backup \
+			--verbose \
+			--files-from ~/.config/restic/backup.files \
+			--exclude-file ~/.config/restic/exclude.files
+		
+		echo "Forgetting good old times..."
+		restic forget \
+			--prune \
+			--keep-last 3 \
+			--keep-daily 14 \
+			--keep-monthly 6 \
+			--keep-yearly 1000
 
-	set backup_end_time (date +%s)
-	set backup_elapsed_time (math --scale=1 \($backup_end_time - $backup_start_time\) / 60)
+		restic check
 
-	# TODO: error handling of backup command
+		set backup_end_time (date +%s)
+		set backup_elapsed_time (math --scale=1 \($backup_end_time - $backup_start_time\) / 60)
 
-	echo "Backup finished in $backup_elapsed_time minutes."
-	notify-send -i drive-harddisk -c transfer.complete "Backup finished in $backup_elapsed_time minutes."
+		# TODO: error handling of backup command
+
+		echo "Backup finished in $backup_elapsed_time minutes."
+		notify-send -i drive-harddisk -c transfer.complete "Backup finished in $backup_elapsed_time minutes."
+	end
 else
 	exit 1
 end
